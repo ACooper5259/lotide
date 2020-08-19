1feb197ec2564046ddee962ab79fa30a5402df23
Audrey Cooper <audrey_cooper@me.com>
2020-08-19 04:33:48 +0000

    final work on without.js

---
+++ without.js
@@ -0,0 +1,37 @@
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length){
+    return false;
+  } else {
+    let finalArray = []
+    for (let x = 0; x < array1.length; x++) {
+      if (array1[x] === array2[x]){
+        finalArray.push(array1[x])
+      }
+    }
+  
+    if (finalArray.length === array1.length){
+      return true;
+    } else {
+      return false
+    }
+  }  
+}
+
+const assertArrayEqual = function(array1, array2) {
+  eqArrays(array1, array2) ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
+};
+
+
+const without = function(source, itemsToRemove) {
+  let resultArray = []
+  for (x = 0; x < itemsToRemove.length; x ++){ 
+    if (source[x] !== itemsToRemove[x]) {
+       resultArray.push(source[x])
+    }
+  }
+  console.log (resultArray)
+  return resultArray
+}
+
+assertArrayEqual(without(["1", "2", "3"], [1, 2, '3']), ['1', '2'])
+assertArrayEqual(without(["Hello", "World"], ["world"]), ["Hello"])
