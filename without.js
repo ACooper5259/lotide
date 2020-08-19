0e13345d1294f75dc201d1dcd83790b5e0e928c0
Audrey Cooper <audrey_cooper@me.com>
2020-08-19 04:43:00 +0000

    eslint without.js

--- without.js
+++ without.js
@@ -1,21 +1,21 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length){
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    let finalArray = []
+    let finalArray = [];
     for (let x = 0; x < array1.length; x++) {
-      if (array1[x] === array2[x]){
-        finalArray.push(array1[x])
+      if (array1[x] === array2[x]) {
+        finalArray.push(array1[x]);
       }
     }
   
-    if (finalArray.length === array1.length){
+    if (finalArray.length === array1.length) {
       return true;
     } else {
-      return false
+      return false;
     }
-  }  
-}
+  }
+};
 
 const assertArrayEqual = function(array1, array2) {
   eqArrays(array1, array2) ? console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`) : console.log(`❌❌❌ Assertion Failed: ${array1} !== ${array2}`);
@@ -23,15 +23,14 @@ const assertArrayEqual = function(array1, array2) {
 
 
 const without = function(source, itemsToRemove) {
-  let resultArray = []
-  for (x = 0; x < itemsToRemove.length; x ++){ 
+  let resultArray = [];
+  for (let x = 0; x < itemsToRemove.length; x ++) {
     if (source[x] !== itemsToRemove[x]) {
-       resultArray.push(source[x])
+      resultArray.push(source[x]);
     }
   }
-  console.log (resultArray)
-  return resultArray
-}
+  return resultArray;
+};
 
-assertArrayEqual(without(["1", "2", "3"], [1, 2, '3']), ['1', '2'])
-assertArrayEqual(without(["Hello", "World"], ["world"]), ["Hello"])
+assertArrayEqual(without(["1", "2", "3"], [1, 2, '3']), ['1', '2']);
+assertArrayEqual(without(["Hello", "World"], ["world"]), ["Hello"]);
