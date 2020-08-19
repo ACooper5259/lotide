4bbdae7a41f54165f7c6a23b21992c887eb1ede7
Audrey Cooper <audrey_cooper@me.com>
2020-08-19 21:46:13 +0000

    new countOnly file

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+//all items: an array of strings to look through
+//items to count: an object of keys with values true if to be counted and false otherwise
+const countOnly = function(allItems, itemsToCount) {
+  const result = {}
+  
+  for (const item of allItems) {
+    if (itemsToCount[item]){
+      if (result[item]){
+        result[item] += 1
+      } else {
+        result[item] = 1
+      }
+    }
+  }
+  return result;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
