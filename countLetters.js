7209cd0e518188d06b02b3ca67e3f8b2d3b808e2
Audrey Cooper <audrey_cooper@me.com>
2020-08-19 22:05:31 +0000

    add countLetters file

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const countLetters = function (sentence){
+  const result = {}
+  for (let char of sentence){
+    if (char != ' ') {
+      if (result[char]) {
+        result[char] += 1
+      } else {
+        result[char] = 1
+      }
+    }
+  }
+
+  return result
+}
+
+const result1 = countLetters('Lighthouse Labs')
+
+assertEqual(result1['L'], 2);
+assertEqual(result1['l'], undefined);
+assertEqual(result1['s'], 2);
+assertEqual(result1['b'], 1);
