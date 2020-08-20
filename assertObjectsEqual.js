640723b424b644fd7d7432586a8e0a490fac4ba4
Audrey Cooper <audrey_cooper@me.com>
2020-08-19 22:03:52 -0700

    add inspect library to assertObjectEqual

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -38,7 +38,8 @@ const eqArrays = function(array1, array2) {
 
 
 const assertObjectEqual = function(object1, object2) {
-  eqObjects(object1, object2) ? console.log(`✅✅✅ Assertion Passed: ${object1} === ${object2}`) : console.log(`❌❌❌ Assertion Failed: ${object1} !== ${object2}`);
+  const inspect = require('util').inspect;
+  eqObjects(object1, object2) ? console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`) : console.log(`❌❌❌ Assertion Failed: ${inspect(object1)} !== ${inspect(object2)}`);
 };
 
 assertObjectEqual({ a: "1", b: "2" },{ b: "2", a: "1" });
