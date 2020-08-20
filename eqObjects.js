8ea80c8c6cd8db997ca6d100a1c32d349e10b2f5
Audrey Cooper <audrey_cooper@me.com>
2020-08-19 20:21:02 -0700

    improved eqObjects with litterals

--- eqObjects.js
+++ eqObjects.js
@@ -10,17 +10,13 @@ const eqObjects = function(object1, object2) {
   if (object2Keys.length !== object1Keys.length) {
     return false;
   } else {
-    for (let element in object1) {
-      console.log(object1[element])
-      if (object1[element] === object2[element]){
-        return true
-      } else {
+    for (let element of object1Keys) {
+      if (!(object1[element] === object2[element])){
         return false
       }
     }
+    return true
   }
- 
-
 };
 
 const ab = { a: "1", b: "2" };
@@ -29,3 +25,10 @@ assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false); // => false
