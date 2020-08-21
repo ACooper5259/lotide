eafacdf92c0946815bfa2fab6733d78c00418cae
Audrey Cooper <audrey_cooper@me.com>
2020-08-20 18:38:01 -0700

    add findKey.js

---
+++ findKey.js
@@ -0,0 +1,40 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(object, callback) {
+  for (key in object) {
+    if (callback(object[key])) {
+      return key;
+    } 
+  }
+}
+
+assertEqual
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma');
+
+assertEqual(findKey({
+  "Steve":  { grade: 68 },
+  "Doug":   { grade: 33 },
+  "Jenn":   { grade: 90 },
+  "Stephanie":   { grade: 49 },
+ 
+}, x => x.grade <= 50), 'Doug');
+
+assertEqual(findKey({
+  "Steve":  { favouriteColor: "blue" },
+  "Doug":   { favouriteColor: "red" },
+  "Jenn":   { favouriteColor: "green" },
+  "Stephanie":   { favouriteColor: "black" },
+ 
+}, x => x.favouriteColor === 'black'), 'Stephanie');
