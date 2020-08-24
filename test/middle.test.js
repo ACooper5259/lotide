41a3d744cddd740d5ba0fbd9ddf8610db0f9c8b7
Audrey Cooper <audrey_cooper@me.com>
2020-08-24 14:08:30 -0700

    middle.test.js added mocha and chai for testing

--- test/middle.test.js
+++ test/middle.test.js
@@ -1,6 +1,30 @@
 const middle = require("../middle");
-const assertArrayEqual = require("../assertArrayEqual");
+const assert = require("chai").assert;
 
+describe("#middle", () => {
+  it("returns [5, '7'] when called with [1, 1, 5, '7', 98, 3]", () => {
+    const actual = middle([1, 1, 5, "7", 98, 3]);
+    const expected = [5, "7"];
+    assert.deepEqual(actual, expected);
+  });
+  it("returns [7] when called with [1, 5, 7, '57', '0']", () => {
+    const actual = middle([1, 1, 5, "7", 98, 3]);
+    const expected = [5, "7"];
+    assert.deepEqual(actual, expected);
+  });
+  it("returns ['morning', 'Victoria'] when called with ['Good', 'morning', 'Victoria', 'bootcampers']", () => {
+    const actual = middle(["Good", "morning", "Victoria", "bootcampers"]);
+    const expected = ["morning", "Victoria"];
+    assert.deepEqual(actual, expected);
+  });
+  it("returns [] when called with ['Good', 'morning']", () => {
+    const actual = middle(["Good", "morning"]);
+    const expected = [];
+    assert.deepEqual(actual, expected);
+  });
+});
+
+/*
 assertArrayEqual(middle([1, 1, 5, "7", 98, 3]), [5, "7"]);
 assertArrayEqual(middle([1, 5, 7, "57", "0"]), [7]);
 assertArrayEqual(middle(["Good", "morning", "Victoria", "bootcampers"]), [
@@ -12,3 +36,4 @@ assertArrayEqual(
   ["Victoria"]
 );
 assertArrayEqual(middle(["Good", "morning"]), []);
+*/
