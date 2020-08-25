ef0442aa48d7b9ed4daf9901f5efb4c1beef45a5
Audrey Cooper <audrey_cooper@me.com>
2020-08-24 17:03:13 -0700

    added missing functions to index file

--- index.js
+++ index.js
@@ -2,10 +2,36 @@
 const head = require("./head"); // import the function
 const tail = require("./tail");
 const middle = require("./middle");
+const assertArrayEqual = require("./assertArrayEqual");
+const assertEqual = require("./assertEqual");
+const assertObjectEqual = require("./assertObjectsEqual");
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const eqArrays = require("./eqArrays");
+const eqObjects = require("./eqObjects");
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const letterPositions = require("./letterPositions");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
+const without = require("./without");
 
 // create the module.exports object that will contain the functions
 module.exports = {
   head: head,
   tail: tail,
   middle: middle,
+  assertArrayEqual: assertArrayEqual,
+  assertArrayEqual: assertEqual,
+  assertObjectEqual: assertObjectEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without,
 };
