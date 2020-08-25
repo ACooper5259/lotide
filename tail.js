01dffca77d097d69757bef882a2882fe0b9769fd
Audrey Cooper <audrey_cooper@me.com>
2020-08-24 17:42:30 -0700

    removed assert equal from tail.js

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function (actual, expected) {
-  actual === expected
-    ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-    : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const tail = function (array) {
   return array.slice(1);
 };
